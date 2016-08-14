68ac0774f2d319cb67c934e036e72ed75138a573
Giovanny Eduardo Figueroa Garcia <gfiguero7183@ean.edu.co>
2016-08-13 19:20:28 -0500

    Add files via upload
    Se agrega diseño de portada con el muñeco que simboliza el ahorcado

--- Proyecto Ahorcado.cpp
+++ Proyecto Ahorcado.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
-
+#define Banner 	cout<<"\t\t\t\t** JUEGO AHORCADO **	\n\n"
 
 using namespace std;
 
 void menu()
 {
-	cout<<"\t\t\t\t** JUEGO AHORCADO **\n\n";
+	Banner;
+	cout<<"\t\t\t\t     ==========		\n";
+	cout<<"\t\t\t\t     |        |		\n";
+	cout<<"\t\t\t\t     |       ###		\n";
+	cout<<"\t\t\t\t     |      #####	\n";
+	cout<<"\t\t\t\t     |       ###		\n";
+	cout<<"\t\t\t\t     |        # 		\n";
+	cout<<"\t\t\t\t     |     #######	\n";
+	cout<<"\t\t\t\t     |        #		\n";
+	cout<<"\t\t\t\t     |        #		\n";
+	cout<<"\t\t\t\t     |       # #		\n";
+	cout<<"\t\t\t\t     |      #   #	\n";
+	cout<<"\t\t\t\t     |     ##   ## 	\n";
+	cout<<"\t\t\t\t     |				\n";
+	cout<<"\t\t\t\t =========			\n\n";
 	
-	cout<<"1. JUGAR"<<endl;
-	cout<<"2. INSTRUCCIONES"<<endl<<endl;
-	cout<<"9. SALIR"<<endl;
+	cout<<"\t\t1. JUGAR\n";
+	cout<<"\t\t2. INSTRUCCIONES\n\n";
+	cout<<"\t\t9. SALIR\n";
 	
-	cout<<endl<<"INGRESE OPCION: ";
+	cout<<endl<<"\t\tINGRESE OPCION: ";
 }
 
 void instrucciones()
 {
 	system("cls");
-	cout<<"\t\t\t\t ** INSTRUCCIONES **\n\n";
-	cout<<"EL JUEGO AHORCADO FUNCIONA DE LA SIGUIENTE FORMA:"<<endl;
-	cout<<"XXXXXXXXXXXXXXXXXXXXXXXXXXXX"<<endl<<endl;
+	Banner;
+	cout<<"EL JUEGO AHORCADO FUNCIONA DE LA SIGUIENTE FORMA:\n\n";
+	cout<<"El supervisor ingresa la palabra para adivinar. \n\n";
+	cout<<"El jugador pide una letra. \n\n";
+	cout<<"* \tSi acierta, se escribe la letra en todas las casillas de la misma letra.\n\n";	
+	cout<<"* \tSi no acierta, se pone la letra bajo la horca \n\ty se dibuja una parte del cuerpo.\n\n";
+	cout<<"* \tGana el jugador si descubre la palabra o frase oculta\n\n";
+	cout<<"* \tPierde si se dibuja todo el cuerpo en la horca\n\n";
+	cout<<"** \tEl cuerpo se dibuja en 6 partes (cabeza, tronco y extremidades),\n\tpor lo que el jugador tiene 6 posibilidades de fallar.\n\n\n";
 }
 
 int juego()
@@ -27,11 +47,16 @@ int juego()
 	string palabra;
 	int x;
 	system("cls");
-	cout<<"\t\t\t\t ** AHORCADO **\n\n";
-	cout<<"Digite la palabra a descubrir:  ";
+	Banner;
+	cout<<"\nDigite la palabra a descubrir:  ";
 	cin>>palabra;
 	x=palabra.length();
-	cout<<"\t\t\** la palabra es: **\n\n"<<palabra<<"** tamaño: "<<x<<" **\n\n";
+	cout<<"\n\n\n\n";
+	system("pause");
+	system("cls");
+	Banner;	
+	cout<<"\t\t ** la palabra es: "<<palabra<<" tamaño: "<<x<<" **\n\n";
+
 	return 0;
 }
 
